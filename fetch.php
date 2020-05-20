fcb4348f103df9871039c776d6fe895901b9a4d9
Dominik Peters <gitkraken@d-peters.com>
2020-05-20 21:22:41 +0200

    refactor all the things

--- fetch.php
+++ fetch.php
@@ -1,85 +1,106 @@
 <?php
-$start = microtime(true);
 const MAX = 432000;
 const LINK = "https://dmginc.gg/di_custom/token-processing/search/SubmitHandle.php";
 
-/**
- * @param $a array
- * @param $b array
- * @return array
- */
-function getLogs($a, $b)
-{
-	$ch = curl_init(LINK);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_HEADER, "Content-Type: application/x-www-form-urlencoded");
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "data=" . json_encode([$a, $b]));
-	$response = curl_exec($ch);
-	curl_close($ch);
-	return json_decode($response);
-}
-
-/**
- * @param $bp array
- * @return array
- */
-function modify($bp)
-{
-	$ret = [$bp, $bp, $bp, $bp];
-	$ret[0]["val"] = "Recruit Token";
-	$ret[1]["val"] = "Apprentice Recruit Token";
-	$ret[2]["val"] = "Journeyman Recruit Token";
-	$ret[3]["val"] = "Master Recruit Token";
-	return $ret;
-}
-
-$values = [];
-$data = strtolower(json_decode($_POST["data"]));
-$head = [
-	"qcid" => "qc_1",
-	"key" => "member_name",
-	"operator" => "0",
-	"concat" => "1",
-	"val" => $data,
-	"position" => "0"
-];
-$body = [
-	"qcid" => "qc_2",
-	"key" => "title",
-	"operator" => "0",
-	"concat" => "2",
-	"val" => null,
-	"position" => "2"
+const TOKEN_TYPES = [
+    "Recruit Token",
+    "Apprentice Recruit Token",
+    "Journeyman Recruit Token",
+    "Master Recruit Token",
 ];
 
-foreach (modify($body) as $value) $values = array_merge($values, getLogs($head, $value));
-
-usort($values, fn($a, $b) => $a->event_date - $b->event_date);
-
-$length = count($values);
-$ret = [];
-$tmp = [];
-
-for ($i = 0; $i < $length; ++$i) {
-	$time = date($values[$i]->event_date);
-	for ($j = $i; $j < $length; ++$j) {
-		if (date($values[$j]->event_date) - $time < MAX) {
-			array_push($tmp, $values[$j]);
-		} else {
-			if (count($tmp) >= count($ret)) $ret = $tmp;
-			$tmp = [];
-			break;
-		}
-	}
+function getTokenLogsForName(string $name): array
+{
+    $tokenLogs = [];
+    foreach (TOKEN_TYPES as $tokenType) {
+        $tokenLogs = array_merge($tokenLogs, getLogsForNameAndTokenType($name, $tokenType));
+    }
+    return $tokenLogs;
 }
+
+function getLogsForNameAndTokenType(string $name, string $tokenType): array
+{
+    $ch = curl_init(LINK);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_HEADER, "Content-Type: application/x-www-form-urlencoded");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "data=" . json_encode(
+            [
+                [
+                    "qcid" => "qc_1",
+                    "key" => "member_name",
+                    "operator" => "0",
+                    "concat" => "1",
+                    "val" => $name,
+                    "position" => "0"
+                ],
+                [
+                    "qcid" => "qc_2",
+                    "key" => "title",
+                    "operator" => "0",
+                    "concat" => "2",
+                    "position" => "2",
+                    "val" => $tokenType
+                ]
+            ]
+        )
+    );
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($response);
+}
+
+function sortByEventDate(stdClass $a, stdClass $b)
+{
+    return $a->event_date - $b->event_date;
+}
+
+function getLongestTokenLogStreak(array $tokenLogs): array
+{
+    $tokenLogStreak = [];
+
+    for ($count = count($tokenLogs), $i = 0; $i < $count; $i++) {
+        $tokenLogsUntil = getTokenLogsUntil(
+            array_slice($tokenLogs, $i),
+            ((int)$tokenLogs[$i]->event_date + MAX)
+        );
+        if (count($tokenLogStreak) < count($tokenLogsUntil)) {
+            $tokenLogStreak = $tokenLogsUntil;
+        }
+    }
+
+    return $tokenLogStreak;
+}
+
+function getTokenLogsUntil(array $logs, int $until): array
+{
+    $tokenLogs = [];
+    foreach ($logs as $log) {
+        if ($log->event_date < $until) {
+            $tokenLogs[] = $log;
+        } else {
+            break;
+        }
+    }
+    return $tokenLogs;
+}
+
+
+$start = microtime(true);
+$tokenLogs = getTokenLogsForName(
+    strtolower(
+        filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING)
+    )
+);
+
+usort($tokenLogs, 'sortByEventDate');
 
 if (microtime(true) - $start < 4) {
-	usleep(rand(5, 15) * 1e5);
+    usleep(rand(5, 15) * 1e5);
 }
 
 header("Content-Encoding: gzip");
-echo gzencode(json_encode($ret));
+echo gzencode(json_encode(getLongestTokenLogStreak($tokenLogs)));
--- index.js
+++ index.js
@@ -5,55 +5,59 @@
 		return document.querySelectorAll(e);
 	}
 
-	let active = false;
-
 	async function postValues(data) {
 		return await fetch("./fetch.php", {
 			method: "POST",
-			body: "data=" + JSON.stringify(data),
+			body: "name=" + data,
 			headers: {
 				"Content-Type": "application/x-www-form-urlencoded"
 			}
-		}).then((data) => {
-			return data.text();
-		});
+		}).then(data => data.json());
 	}
+
+	function getTier(length) {
+		if (length < 5) {
+			return 0;
+		} else if (length < 12) {
+			return 1;
+		} else if (length < 25) {
+			return 2;
+		} else if (length < 50) {
+			return 3;
+		} else if (length < 100) {
+			return 4;
+		}
+		return 5;
+	}
+
+	let active = false;
 
 	$("form")[0].onsubmit = async e => {
 		e.preventDefault();
 		if (active) return;
-		let holder = $(".holder")[0];
-		let container = $(".container")[0];
+		const holder = $(".holder")[0];
+		const container = $(".container")[0];
 
 		active = true;
 		holder.style.opacity = 1;
 		holder.style.zIndex = 10;
 		container.style.filter = "blur(2px)";
 
-		let json = JSON.parse(await postValues($("input")[0].value));
-		let tier, length = json.length;
-		console.log(json);
+		const json = await postValues($("input")[0].value);
+		// console.log(json);
 
-		if (length < 5) {
-			tier = 0;
-		} else if (length < 12) {
-			tier = 1;
-		} else if (length < 25) {
-			tier = 2;
-		} else if (length < 50) {
-			tier = 3;
-		} else if (length < 100) {
-			tier = 4;
-		} else {
-			tier = 5;
-		}
-
-		$("p")[0].innerHTML = `<img src="assets/img/ok${tier}.png" alt="OK${tier}" style="vertical-align: middle"> Your longest streak is ${length} tokens`;
+		$("p")[0].innerHTML = `<img src="assets/img/ok${(getTier(json.length))}.png" alt="OK${(getTier(json.length))}" style="vertical-align: middle"> Your longest streak is ${(json.length)} tokens`;
 		let str = "<th>ID</th><th>Token</th><th>Reason</th><th>Issuer</th><th>Date</th>";
-		json.forEach((token) => {
-			let values = [token["id"], token["title"], token["reason"], token["giver_name"], new Date(token["event_date"] * 1e3).toLocaleString("en-GB")];
+		json.forEach(token => {
+			const values = [
+				token["id"],
+				token["title"],
+				token["reason"],
+				token["giver_name"],
+				new Date(token["event_date"] * 1e3).toLocaleString("en-GB")
+			];
 			str += `<tr><td>${values.join("</td><td>")}</td></tr>`;
-		})
+		});
 		$("table")[0].innerHTML = str;
 
 		active = false;
